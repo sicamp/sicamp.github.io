147efee1ec9db4600d72284ebdc04459ae3a7ed7
isqua <isqua@isqua.ru>
2016-06-02 23:55:21 +0300

    Вынести данные на отдельный уровень; entrance-bonus

--- common.blocks/entrance-bonus/entrance-bonus.bh.js
+++ common.blocks/entrance-bonus/entrance-bonus.bh.js
@@ -38,7 +38,7 @@ module.exports = (bh) => {
                             },
                             text
                         ]
-                    }
+                    };
                 })
             ]
         });

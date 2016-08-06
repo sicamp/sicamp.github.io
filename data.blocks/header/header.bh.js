ac297df91ec0d13e608c43e632dc7eced007ecfc
Kirill Simonov <kirillsimonov@gmail.com>
2016-08-06 20:47:33 +0500

    Вещи в нотификации

--- data.blocks/header/header.bh.js
+++ data.blocks/header/header.bh.js
@@ -1,10 +1,11 @@
 module.exports = (bh) => {
     bh.match('header', (ctx) => {
         const letsgo = bh.lib.resolve('letsgo');
+        const things = bh.lib.resolve('things');
 
         ctx.param(
             'notification',
-            `Подготовьте <a href="${letsgo}#documents">документы</a>, купите билеты до Перми,
+            `Подготовьте <a href="${letsgo}#documents">документы</a>, соберите <a href="${things}">вещи</a>,
             и <a href="${letsgo}#departure">приезжайте в лагерь</a> 9-го августа!`
         );
     });

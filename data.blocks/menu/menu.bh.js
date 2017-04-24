94dab40f4da50ed5e9ba9c18389877cd3e136892
isqua <isqua@isqua.ru>
2017-04-25 00:18:55 +0300

    Добавить ссылки в меню и футер

--- data.blocks/menu/menu.bh.js
+++ data.blocks/menu/menu.bh.js
@@ -1,5 +1,14 @@
 module.exports = (bh) => {
     bh.match('menu', (ctx) => {
-        ctx.param('items', []);
+        ctx.param('items', [
+            {
+                url: bh.lib.resolve('letsgo'),
+                content: 'Как попасть в лагерь'
+            },
+            {
+                url: bh.lib.resolve('entrance'),
+                content: 'Вступительная работа'
+            }
+        ]);
     });
 };
